e661c375afd273e714661ac4a5892ba8f844c5de
apta-tamilarasi <athinarayanantamilarasi@gamil.com>
2023-09-11 21:38:27 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,10 @@
+let a=parseInt(prompt("Enter the number"))
+
+if(a%9==0){
+
+	document.write(`Given number ${a} is divisible by 9`)
+}
+
+else{
+	document.write(`Given number ${a} is not divisible by 9`)
+}
